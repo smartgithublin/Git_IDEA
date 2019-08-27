603ea15a8ca9067668960924021d0903f18c5642
admin <admin@163.com>
2019-08-27 23:09:33 +0800

    第一次提交GitTest.class类

---
+++ resp/src/main/java/com/it/lkw/test/GitTest.java
@@ -0,0 +1,14 @@
+package com.it.lkw.test;
+
+public class GitTest {
+    public static void main(String[] args) {
+
+        System.out.println("Git hello  !!!");
+        System.out.println("Git hello  !!!");
+        System.out.println("Git hello  !!!");
+        System.out.println("Git hello  !!!");
+        System.out.println("Git hello  !!!");
+        System.out.println("Git hello  !!!");
+        System.out.println("Git hello  !!!");
+    }
+}
